df944e7f8fae191db9c9df31313dd8c67206e108
Aditya Dewan <aditya.dewan124@gmail.com>
2023-08-01 17:06:47 -0400

    Added functionality to adjust dataloader length.

--- distillation/hintonDistiller.py
+++ distillation/hintonDistiller.py
@@ -4,12 +4,13 @@ from distillation.utils import Accuracy, AverageMeter, Hook
 from distillation.baseDistiller import BaseDistiller
 
 class HintonDistiller(BaseDistiller):
-    def __init__(self, alpha, studentLayer=-2, teacherLayer=-2):
+    def __init__(self, alpha, studentLayer=-2, teacherLayer=-2, n_samples_per_epoch = 390):
         super(HintonDistiller, self).__init__()
         
         self.alpha = alpha
         self.studentLayer = studentLayer
         self.teacherLayer = teacherLayer
+        self.n_samples_per_epoch = n_samples_per_epoch
         
         # Register hooks
         self.studentHook = Hook()
@@ -35,7 +36,7 @@ class HintonDistiller(BaseDistiller):
         lossMeter = AverageMeter()
         accMeter = AverageMeter()
         
-        for _, (data, target) in enumerate(dataloader):
+        for num_iter, (data, target) in enumerate(dataloader):
             data, target = data.to(device), target.to(device)
 
             # Calculate logits
@@ -58,6 +59,9 @@ class HintonDistiller(BaseDistiller):
             # Save metrics
             lossMeter.update(batchLoss.item(), n=len(data))
             accMeter.update(accuracy(nn.functional.softmax(sLogits, dim=1), target), n=len(data))
+
+            #If the number of iterations = n_samples per epoch, break (parsed through the desired dataloader length)
+            if num_iter == n_samples_per_epoch - 1: break
         
         return {'Train/Loss': lossMeter.avg,
                 'Train/Accuracy': accMeter.avg}
